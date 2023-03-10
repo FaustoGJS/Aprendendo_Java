cedaa7612ccc7eb5f2c7d20b1444b95ae1513bd1
Fausto Garcia Jacinto Silva <126115455+FaustoGJS@users.noreply.github.com>
2023-03-10 07:41:54 -0300

    desafio 1 -imprimindo números pares de 0 a 100 / desafio 2 - colocar espaço entre letras de uma frase

--- udemy/Aprenda JAVA em 7 dias/Dia 4/src/Main.java
+++ udemy/Aprenda JAVA em 7 dias/Dia 4/src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         desafio1();
+        desafio2();
 
     }
     public static void desafio1(){
+        System.out.println("\n----------------------\nDESAFIO 1");
         System.out.println("\nIMPRIMIR DE 0 ATE 100 APENAS NUMEROS PARES\nSOLUÇÃO 1");
         for (int i = 0;i<=100;i+=2){
             System.out.print(i+" ");
@@ -13,6 +17,17 @@ public class Main {
             if(i%2==0){
                 System.out.print(i+" ");
             }
+        }
+    }
+    public static void desafio2(){
+        System.out.println("\n----------------------\nDESAFIO 2");
+        System.out.println("imprimir qualquer frase com espaços entre as letras");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite uma frase: ");
+        String frase = input.nextLine();
+        System.out.println("\n"+frase);
+        for(int i =0;i < frase.length();i++){
+            System.out.print(frase.charAt(i)+" ");
         }
     }
 }
